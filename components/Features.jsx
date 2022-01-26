c128cab15f36c2299c51132d0cfc753761b6eee6
en3rypt <sanjithnathan@gmail.com>
2022-01-27 00:06:45 +0530

    added mobile view for main page,features section

--- components/Features.jsx
+++ components/Features.jsx
@@ -8,23 +8,23 @@ import rating from 'components/assets/rate.svg'
 const Features = () => {
   return (
     <div className='bg-gradient-to-r from-gradient1l to-gradient1r relative -top-2'>
-      <div className='flex items-center justify-around mx-5 mb-20'>
-        <div className='w-1/2'>
-          <div className='border-t-4 border-dark w-52'></div>
+      <div className='flex items-center justify-around mx-10 md:mx-5 mb-20 flex-col md:flex-row text-center md:text-left'>
+        <div className='md:w-1/2'>
+          <div className='border-t-4 border-dark w-32 md:w-52 m-auto md:m-0'></div>
           <h3 className='font-kanit font-bold text-white text-4xl pb-3 pt-2'>Track Your Watched Movies</h3>
-          <p className='font-kanit text-white text-xl'>
+          <p className='font-kanit text-white text-xl pb-10 md:pb-0 md:mr-10'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam suspendisse scelerisque viverra semper felis,
             duis turpis. Elit lacus proin nibh at.
           </p>
         </div>
-        <Image src={trackMovies} width='280' height='210' />
+        <Image src={trackMovies} width='280' height='210' ca />
       </div>
 
-      <div className='flex items-center justify-around flex-row-reverse mx-5 mb-20'>
-        <div className='w-1/2 text-right flex flex-col items-end'>
-          <div className='border-t-4 border-dark w-52'></div>
+      <div className='flex items-center justify-around  mx-10 md:mx-5 md:mb-20 flex-col md:flex-row-reverse text-center md:text-right'>
+        <div className='md:w-1/2 md:flex md:flex-col md:items-end'>
+          <div className='border-t-4 border-dark w-32 md:w-52 m-auto md:m-0'></div>
           <h3 className='font-kanit font-bold text-white text-4xl pb-3 pt-2'>Add To Favourites</h3>
-          <p className='font-kanit text-white text-xl'>
+          <p className='font-kanit text-white text-xl pb-10 md:pb-0 md:ml-10'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam suspendisse scelerisque viverra semper felis,
             duis turpis. Elit lacus proin nibh at.
           </p>
@@ -32,16 +32,16 @@ const Features = () => {
         <Image src={addToFavorutes} width='350' height='203' />
       </div>
 
-      <div className='flex items-center justify-around mx-5 pb-6'>
-        <div className='w-1/2'>
-          <div className='border-t-4 border-dark w-52'></div>
+      <div className='flex items-center justify-around mx-10 md:mx-5 md:mb-20 flex-col md:flex-row text-center md:text-left'>
+        <div className='md:w-1/2'>
+          <div className='border-t-4 border-dark w-32 md:w-52 m-auto md:m-0'></div>
           <h3 className='font-kanit font-bold text-white text-4xl pb-3 pt-2'>Rate Your Watched Movies</h3>
-          <p className='font-kanit text-white text-xl'>
+          <p className='font-kanit text-white text-xl pb-10 md:pb-0 md:mr-10'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam suspendisse scelerisque viverra semper felis,
             duis turpis. Elit lacus proin nibh at.
           </p>
         </div>
-        <Image src={rating} width='400' height='213' />
+        <Image src={rating} width='350' height='186' />
       </div>
       <Wave
         className='bg-gradient-to-r from-gradient1l to-gradient1r  relative top-3'
--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -5,20 +5,22 @@ import mainpageimg from 'components/assets/main.svg'
 const MainPage = () => {
   return (
     <div className=''>
-      <div className='flex items-center justify-around '>
-        <div className='w-1/2'>
-          <h4 className='font-kanit text-xl'>WELCOME TO MOVIE LIST</h4>
-          <h1 className='font-kanit font-bold text-primary text-8xl'>Lorem ipsum dolor sit.</h1>
-          <div className='mt-10'>
-            <button className='text-white bg-primary border border-blue-500 mr-7 rounded font-kanit px-6 py-2 text-3xl  transition-all duration-100'>
+      <div className='flex items-center justify-around flex-col md:flex-row text-center md:text-left'>
+        <div className='mx-5 h-[88vh] md:h-full flex flex-col text-center justify-center md:justify-start md:text-left'>
+          <h4 className='font-kanit text-xl'>WELCOME TO MY MOVIE LIST</h4>
+          <h1 className='font-kanit font-bold text-primary text-5xl  md:text-5xl lg:text-6xl'>
+            Lorem ipsum dolor sit.
+          </h1>
+          <div className='mt-10 flex justify-between flex-col  items-center sm:flex-row sm:justify-center md:justify-start'>
+            <button className='text-white bg-primary border border-blue-500 rounded font-kanit text-2xl sm:text-3xl px-6 py-2   sm:mr-5'>
               Sign Up
             </button>
-            <button className='text-primary bg-transparent border border-blue-500 ml-7 rounded font-kanit px-6 py-2 text-3xl hover:text-white hover:border-transparent hover:bg-primary transition-all duration-100'>
+            <button className='text-primary bg-transparent border border-blue-500  rounded font-kanit  text-2xl sm:text-3xl mt-3 sm:mt-0 px-7 py-2 hover:text-white hover:border-transparent hover:bg-primary transition-all duration-100'>
               Sign In
             </button>
           </div>
         </div>
-        <div className=''>
+        <div className='mx-10 md:mx-0 md:mr-10'>
           <Image src={mainpageimg} width='500' height='500' />
         </div>
       </div>
