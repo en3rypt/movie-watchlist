f7f215943be97aee1a097dbfaf08b152a648a6d4
en3rypt <sanjithnathan@gmail.com>
2022-01-27 12:09:11 +0530

    favicon change

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel='icon' href='/favicon.ico' />
           <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
           <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
@@ -18,7 +19,8 @@ class MyDocument extends Document {
           <meta name='msapplication-TileColor' content='#0099ff' />
           <meta name='theme-color' content='#ffffff' />
 
-          {/* <link rel='icon' href='/favicon.ico' />
+          {/* 
+          <link rel='icon' href='/favicon.ico' />
           <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
           <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
